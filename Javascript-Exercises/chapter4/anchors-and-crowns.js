4acdd7435c8ea03f9a1f43be67a8a495cc013dac
dotecha <apokueo@gmail.com>
2018-01-25 10:15:24 -0500

    diving into chapter 4

---
+++ Javascript-Exercises/chapter4/anchors-and-crowns.js
@@ -0,0 +1,4 @@
+let funds = 50, bets = {}, hand = []; 
+
+// helper functions are functions that perform part of the computation of another function and are used to make programs easier to read 
+// by giving descriptive names to computations. They let you reuse computation, just as with functions in general. 
