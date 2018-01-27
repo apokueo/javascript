30c71f5f94e52544ec6ad4c5e84a79cf64373deb
dotecha <apokueo@gmail.com>
2018-01-27 11:44:02 -0500

    Working on expressions

---
+++ Javascript-Exercises/chapter5/lesson5-expressions.js
@@ -0,0 +1,10 @@
+// declaration statement  
+// multiplication expression resolves to 15
+// = is an assignment expression that assigns the value of 15 to x 
+
+let x, y; 
+console.log(y); 
+y = x = 3 * 5; 
+console.log(y)
+console.log(x)
+console.log(y)
