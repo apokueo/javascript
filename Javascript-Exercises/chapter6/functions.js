a6312a5d3ce76b07dfa3bac42481f104a4a65815
dotecha <apokueo@gmail.com>
2018-01-29 18:52:17 -0500

    Playing with functional args

--- Javascript-Exercises/chapter6/functions.js
+++ Javascript-Exercises/chapter6/functions.js
@@ -18,5 +18,18 @@ o.f = getGreeting
 o.f();
 
 const arry = [1,2,3];
-arr[1] = getGreeting;
-arr[1]();
+arry[1] = getGreeting;
+arry[1]();
+
+console.log('getting into that funky function'); 
+
+function e(x) {
+	console.log(`inside e: x=${x}`); 
+	x = 5; 
+	console.log(`inside e: x=${x} (after assignment)`); 
+} 
+
+var x = 3; 
+console.log(`before alling e: x=${x}`); 
+e(x); 
+console.log(`after calling e: x=${x}`); 
