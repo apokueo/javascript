cd248d41085b4ab7c1adbd4a13472ab10ceeb4a9
dotecha <apokueo@gmail.com>
2018-01-29 20:30:36 -0500

    anonymous functions

---
+++ Javascript-Exercises/chapter6/anonymous-functions.js
@@ -0,0 +1,15 @@
+const h = {
+  name: 'Derpa',
+
+  greetBackwards: function () {
+    const getReverseName = () => {
+      let nameBackwards = '';
+      for( let i=this.name.length-1; i>=0; i--) {
+        nameBackwards += this.name[i];
+      }
+      return nameBackwards;
+    };
+    return `${getReverseName()} si eman ym ,olleH`;
+  },
+};
+h.greetBackwards();
