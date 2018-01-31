0f024bbd6f3a8a508f3472e3dacc9542df78bc92
IAmACoder0816 <vprodigys@gmail.com>
2018-01-31 06:10:08 -0500

    Strict Mode w/in IIFE

--- chapter7/IIFE.js
+++ chapter7/IIFE.js
@@ -16,3 +16,16 @@
 			var spy = "James Bond";
 			return spy;
 		})();
+
+
+
+	(function() {
+		'use strict';
+		// all of your code goes here... it
+		// is executed in strict mode, but
+		// the strict code mode won't contaminate
+		// any other scripts that are combined
+		// with this one
+	})();
+
+//Strict Mode (see above) in the form of an IIFE. 
