f512d59edc0e9459f46522a9ad96f7289253eac7
dotecha <apokueo@gmail.com>
2018-01-31 20:05:24 -0500

    arrays (find)

--- Javascript-Exercises/chapter8/arrays-array_processing.js
+++ Javascript-Exercises/chapter8/arrays-array_processing.js
@@ -1,1 +1,20 @@
 // code praxis for chapter 8 Arrays and Array Processing 
+
+class Person {
+	constructor(name) {
+		this.name = name; 
+		this.id = Person.nextId++; 
+	}
+}
+
+Person.nextId = 0; 
+const jamie = new Person("Jamie"),
+	juliet = new Person("Juliet"),
+	peter = new Person("Peter"),
+	jay = new Person("Jay");
+
+const arr = [jamie, juliet, peter, jay]; 
+
+arr.find(p => p.id === juliet.id); 
+
+arr.find(p => === this.id, juliet); 
