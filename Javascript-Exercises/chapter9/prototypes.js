79cc1d4d9ba8fe0a499045f7bde93c2fe283fb7d
dotecha <apokueo@gmail.com>
2018-02-06 07:52:30 -0500

    prototyping in javascript

--- Javascript-Exercises/chapter9/prototypes.js
+++ Javascript-Exercises/chapter9/prototypes.js
@@ -1,4 +1,8 @@
-// Prototypes are methods that are available on instances of a class
-// Every function has a property 
+/** Prototypes are methods that are available on instances of a class
+	Every function has a property 
+	a class is another name for an object constructor
+	a function's prototype property when you
+all instances of a given class share the same prototype or method **/ 
 
-f(); 
+
+const ca
