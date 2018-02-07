0196f248f8a3efb03b68a29050d6eb6e6fce9f08
IAmACoder0816 <vprodigys@gmail.com>
2018-02-07 16:46:32 -0500

    Initial Commit for JavaScript Recap

---
+++ JS_Exercises_Recap/js_exercise_recap.js
@@ -0,0 +1,37 @@
+var name = "Hoodie";
+//variables
+
+// object literal
+
+	var person {
+		name: "Margaret",
+		age: 30',
+		sex: 'female',
+			address {
+				street: "238 Cornwell Drive",
+				city: "Bear"
+				state: "Pennsylvania"
+			}
+	};
+console.log(person.street);
+
+var fruit = ["oranges", "apples", "limes", "lemons", "peaches"]
+console.log;
+
+const temp = 99;
+console.log(temp);
+
+var greeting = "Hello Dimension"
+console.log(greeting);
+
+//JavaScript uses 3 basic logic structures in programming: sequence, selection, //and iteration. Must Research more thoroughly.
+//
+//ECMAScript is a Standard for a scripting language.  Languages like JavaScript //are based on the ECMASCript standard. ECMA stands for European Computer 
+// Manufacturer's Assiociation.
+//
+// There are 6 possibly 7 primitive data types.  Below are stated:
+// Number, String, Boolean, Null, Undefined, Symbol.
+// The first 3 are Data Types mentioned are known as Primitives.
+// Literals are Values in the simplest explanation.
+//
+
