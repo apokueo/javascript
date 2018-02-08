9585023bdf80c598e3a61c7c23965b3db594ee8e
dotecha <apokueo@gmail.com>
2018-02-08 07:38:12 -0500

    Global Var In Functions - DOM Scripting

---
+++ Javascript-Exercises/chapter9/Glob_Var_In_func.js
@@ -0,0 +1,17 @@
+function func() {
+	var a = 1, 
+            b = 2,
+	    sum = a + b, 
+		myobj = {}, 
+		i,
+		j; 
+	console.log(a); 
+	console.log(b); 
+	console.log(sum); 
+	console.log(myobj); 
+	console.log(i); 
+	console.log(j); 
+	// function body 
+} 
+
+func(); 
