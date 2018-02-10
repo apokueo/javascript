6b0e9a4dc68f8161ff344e4913af778033afca48
dotecha <apokueo@gmail.com>
2018-02-10 16:05:47 -0500

    Extracted function from react

---
+++ React-Fundamentals/app1/script1.js
@@ -0,0 +1,11 @@
+function exampleFunc() { 
+    console.log("I am only an example golem") 
+} 
+
+
+function anotherFunc() { 
+    console.log("Will you listen while I testify? " ) 
+} 
+
+
+module.exports = exampleFunc() 
