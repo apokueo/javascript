7bb14c89ba53889e346cf4069b1601ea2f574250
dotecha <apokueo@gmail.com>
2018-02-10 23:46:16 -0500

    modified script.js / index.html

--- Design-Patterns/app/js/script.js
+++ Design-Patterns/app/js/script.js
@@ -16,3 +16,20 @@ tracks.push({
 	title: 'New track', 
 	duration: 23, 
 }) 
+
+var tracksContainer = document.getElementById('tracks') 
+
+var i, row, cell1, cell2, cell3, cell4
+
+for(i = 0; < tracksContainer.length; i++) { 
+	row = tracksContainer.insertRow(i) 
+	cell1 = row.insertCell(0)
+	cell1 = row.insertCell(1)
+	cell1 = row.insertCell(2)
+	cell1 = row.insertCell(3)
+
+	cell1.innerHTML = tracks[i].id 
+	cell2.innerHTML = tracks[i].title
+	cell1.innerHTML = tracks[i].duration
+
+} 
