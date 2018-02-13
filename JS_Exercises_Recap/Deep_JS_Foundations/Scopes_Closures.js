6b3327459d810d0d2602df95ec08e204b4b1a0fc
IAmACoder0816 <vprodigys@gmail.com>
2018-02-13 16:40:39 -0500

    Updated with newer notes on how to better define a scope with a function.

--- JS_Exercises_Recap/Deep_JS_Foundations/Scopes_Closures.js
+++ JS_Exercises_Recap/Deep_JS_Foundations/Scopes_Closures.js
@@ -4,11 +4,38 @@
 //	Closure
 //	Modules
 	
-	var clickHandler = function() {
+//	var clickHandler = function() {
 		//..
-	};
+//	};
 
-// When naming your function expression, it is advised to name the function, not// like Naming Conventions: but use a specific name for that specific use case.
-// Like naming it verbs on what actions they do or what the end result will be
-// maybe nouns.
-		
+
+// Function Scoping
+
+var foo = "foo";
+
+// ..
+
+var foo = "foo2";
+console.log(foo);	// "foo2"
+
+// ..
+
+console.log(foo);	// "foo2" -- oops!
+
+// Creating a scope is to contain or encapsulate the variable declaration, around line 5 and 6 and to fix it by a function.
+// His fix for this is below.
+
+var foo = "foo";
+
+function bob() {
+	var foo = "foo2";
+	console.log(foo);	// "foo2"
+}
+bob();
+
+console.log(foo);	// "foo" -- phew!
+
+// Scopes are used to prevent collisions with functions or just to protect it from access.
+// JavaScript's unit of Scope is the function. Which means the scope's building blocks are accessible or viewed better within a function.
+
+
