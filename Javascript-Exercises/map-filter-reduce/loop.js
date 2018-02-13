6067f4483faf3f9b3e656a0a4347e6feb7a83f9c
dotecha <apokueo@gmail.com>
2018-02-12 19:48:09 -0500

    loop expanding

--- Javascript-Exercises/map-filter-reduce/loop.js
+++ Javascript-Exercises/map-filter-reduce/loop.js
@@ -1,7 +1,7 @@
-var i = 0; var array = [1, 2, 3, 4, 5, 6];  
+var i = 0; var array = [1, 2, 3, 4, 5, 6]; models = [1,2,3];   
 
 for (i; i < array.length; i++) {
-    if(array.length.indexOf(array[i]) === i) {
+    if(array.indexOf(array[i]) === i) {
         models.push(array[i]); 
     } 
 } 
