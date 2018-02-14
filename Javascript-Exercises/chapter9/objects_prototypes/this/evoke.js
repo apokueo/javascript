de9ba0189ba44010c0d660a78a2589725d6e19c0
dotecha <apokueo@gmail.com>
2018-02-14 12:28:38 -0500

    evoke :: praxis on binding

---
+++ Javascript-Exercises/chapter9/objects_prototypes/this/evoke.js
@@ -0,0 +1,15 @@
+function evocation() { 
+	console.log(this.spirit); 
+} 
+
+var obj = { 
+	art: evocation, 
+	spirit: function() {
+		console.log('King Paiymon') 
+	}, 
+} 
+
+let ether = obj.spirit
+
+let bindFunc = ether.bind(evocation)
+console.log(bindFunc); 
