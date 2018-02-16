3ab5169818ad5a3a6f7ebc74c313603f63bb8421
dotecha <apokueo@gmail.com>
2018-02-15 20:40:54 -0500

    Making changes

---
+++ Javascript-Exercises/scopes-closures/scopes1.js
@@ -0,0 +1,14 @@
+class Employee {
+    constructor(name, position) {
+        var nameObj = function(name, position) {
+           this.name: name,
+           this.position: position,
+           this.evocation: function() {
+               console.log("conjure something beyond")
+        }
+   }
+}
+
+class Mgr extends Employee {
+    constructor(department, staff) { super()
+            var staffObj
