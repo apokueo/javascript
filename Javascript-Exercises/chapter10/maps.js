c7241b579a64d2d5c0174ad684de3c5abc092a50
dotecha <apokueo@gmail.com>
2018-02-15 20:51:48 -0500

    maps

--- Javascript-Exercises/chapter10/maps.js
+++ Javascript-Exercises/chapter10/maps.js
@@ -1,1 +1,22 @@
-                            
+const u1 = { name: 'Cynthia' };
+const u2 = { name: 'Jackson' };
+const u3 = { name: 'Olive' };
+const u4 = { name: 'James' };
+
+
+const userRoles = new Map();
+
+userRoles.set(u1, 'User1');
+userRoles.set(u2, 'User2');
+userRoles.set(u3, 'User3');
+
+userRoles
+    .set(u1, 'User')
+    .set(u2, 'User')
+    .set(u3, 'Admin');
+
+userRoles.get(u2); // User
+
+userRoles.has(u1); // true
+userRoles.get(u1); // true
+
