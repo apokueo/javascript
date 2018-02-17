27e1003e0cdac7fd990eb7feada87ef71ca4d0af
dotecha <apokueo@gmail.com>
2018-02-17 11:38:20 -0500

    Working with namespaces, coercion, best practices

---
+++ Javascript-Exercises/chapter10/sets.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let myArray = [11,22,34, 65, 34];
+let mySet = new Set(myArray);
+
+mySet.add('100');
+mySet.add({a: 1, b:2 });
+mySet.delete(22);
+mySet.clear
+
+
---
+++ mastering/namespaces.js
@@ -0,0 +1,13 @@
+/* cool - Silence */
+
+var coolCat = new com.apokueo.Silence('cat')
+
+var com =  com || {},
+    com.apokueo = com.apokueo || {};
+
+    function com.apokueo.Silence(type) {
+
+
+    }
+
+Silence.prototype.version = '1.00';
