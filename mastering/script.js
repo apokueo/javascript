56628d05fc255b14433ddcfe3481750a3df37473
dotecha <apokueo@gmail.com>
2018-02-17 11:56:04 -0500

    Working with crowbrowswer

---
+++ mastering/script.js
@@ -0,0 +1,11 @@
+document.onload = function() {
+    console.log("document is loaded.");
+}
+
+window.onload = function() {
+    console.log("window is loaded.");
+}
+
+window.onload = function() {
+    console.log("window is loaded. 2");
+}
