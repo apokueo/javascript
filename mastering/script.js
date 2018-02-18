15329d3ad4a12f6d56aa8f24743ecaa928716d09
dotecha <apokueo@gmail.com>
2018-02-18 13:16:01 -0500

    Working with mastering loads

--- mastering/script.js
+++ mastering/script.js
@@ -1,11 +1,24 @@
-document.onload = function() {
-    console.log("document is loaded.");
-}
-
 window.onload = function() {
     console.log("window is loaded.");
 }
 
-window.onload = function() {
-    console.log("window is loaded. 2");
+addOnLoad(function() {
+    console.log("2 window is loaded.");
+});
+
+addOnLoad(function() {
+    console.log("3 window is loaded.");
+});
+
+addOnLoad(function() {
+    console.log("4 window is loaded.");
+});
+
+function addOnLoad(fun) {
+    var last = window.onLoad;
+    window.onload = function() {
+        if(last) last();
+
+        fun();
+    }
 }
