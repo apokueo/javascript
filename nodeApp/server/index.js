6369ee51cdd26724fd439da37deba53e72e3c651
dotecha <apokueo@gmail.com>
2018-02-18 14:28:49 -0500

    Writing routes and preparing for deploy on heroku

--- nodeApp/server/index.js
+++ nodeApp/server/index.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
     res.send({ hi: 'there' });
 });
 
+
+// when HEROKU deploys this app, it has the ability to invoke envars (see if they declared PORT)
+const PORT = process.env.PORT || 5000;
+
+// Tell Heroku what version of node you want it to use
+
 app.listen(5000);
 
 
