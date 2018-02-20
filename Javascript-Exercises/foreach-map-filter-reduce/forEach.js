80425edc4e44cda14c32085f6985cf5ad7eb7e92
dotecha <apokueo@gmail.com>
2018-02-20 17:10:01 -0500

    forEach replaces for Loops

---
+++ Javascript-Exercises/foreach-map-filter-reduce/forEach.js
@@ -0,0 +1,14 @@
+var colors = [ 'red', 'blue', 'green' ]; 
+
+var i = 0; 
+
+for ( ; i < colors.length; i++) {
+    console.log(colors[i]); 
+} 
+
+
+// forEach 
+
+colors.forEach(function(color) {
+    console.log(colors)
+}); 
