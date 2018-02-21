bd83431811298b981049887b76262a7e4c7f120b
dotecha <apokueo@gmail.com>
2018-02-21 00:03:57 -0500

    need to fix forEach3.js

---
+++ Javascript-Exercises/foreach-map-filter-reduce/forEach3.js
@@ -0,0 +1,15 @@
+var data = [1,2,3,4,5], sum = 0
+
+// compute the sum of the array of elements 
+
+data.forEach(function(value {
+    sum += value; 
+}); 
+
+sum 
+
+// Now increment each array element 
+data.forEach(function(v,i,a) {
+    a[i] = v + 1; 
+}); 
+
